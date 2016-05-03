f5c2301db709db897d9d2033772a06fff5d967fb
Christofer Lind <lind.christofer@gmail.com>
2016-05-03 14:52:04 +0200

    full support for two hands

--- test.py
+++ test.py
@@ -3,21 +3,35 @@ import json
 import getPositions
 
 def calculateDistances(rightPalmPosition, rightFingerTipPositions, leftPalmPosition, leftFingerTipPositions):
-	featureVector = []
-	for n in range(0, len(rightFingerTipPositions)):
-		coordinates = rightFingerTipPositions[n]
-		# Extract the distance in each direction
-		x = abs(coordinates[0] - rightPalmPosition[0])
-		y = abs(coordinates[1] - rightPalmPosition[1])
-		z = abs(coordinates[2] - rightPalmPosition[2])
+    #print len(rightPalmPosition), len(rightFingerTipPositions), len(leftPalmPosition), len(leftFingerTipPositions)
+    featureVector = []
+    if rightPalmPosition != [] and rightFingerTipPositions != []:
+        for n in range(0, len(rightFingerTipPositions)):
+            coordinates = rightFingerTipPositions[n]
+            # Extract the distance in each direction
+            x = abs(coordinates[0] - rightPalmPosition[0])
+            y = abs(coordinates[1] - rightPalmPosition[1])
+            z = abs(coordinates[2] - rightPalmPosition[2])
 
-		# Add the euclidian distance to the feature vector
-		featureVector.append(euclidianDistance(x,y,z))
+            # Add the euclidian distance to the feature vector
+            featureVector.append(euclidianDistance(x,y,z))
 
-	return featureVector
+    if (leftPalmPosition != [] and leftFingerTipPositions != []):
+        for n in range(0, len(leftFingerTipPositions)):
+            coordinates = leftFingerTipPositions[n]
+            # Extract the distance in each direction
+            x = abs(coordinates[0] - leftPalmPosition[0])
+            y = abs(coordinates[1] - leftPalmPosition[1])
+            z = abs(coordinates[2] - leftPalmPosition[2])
+
+            # Add the euclidian distance to the feature vector
+            featureVector.append(euclidianDistance(x,y,z))
+
+
+    return featureVector
 
 def euclidianDistance(x,y,z):
-	return math.sqrt(math.pow(x,2) + math.pow(y,2) + math.pow(z,2))
+    return math.sqrt(math.pow(x,2) + math.pow(y,2) + math.pow(z,2))
 
 
 print('---------------------')
