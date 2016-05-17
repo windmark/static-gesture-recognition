69bdb14c26e640d057381a04968863886f773ca2
Christofer Lind <lind.christofer@gmail.com>
2016-05-17 18:00:25 +0200

    gui förbättrat :)

--- states.py
+++ states.py
@@ -1,86 +1,90 @@
 import time, sys
 
 def printout(self, toPrint):
-	#print "\r {:<30s}         ".format(toPrint)
-	sys.stdout.write("\r {:<35s}".format(toPrint))
+	sys.stdout.write("\r {:<50s}".format(toPrint))
 	printCart(self.cart)
 	#sys.stdout.flush()
 
 def printCart(cart):
-	print "{:>25}:{}".format("Cart:",cart)
-	#sys.stdout.write("{:>25}:{}".format("Cart:",cart))
+	print("{:>50}{}".format('Cart: ', ', '.join(cart)))
+	#sys.stdout.write("{:>75}{}".format('Cart: ', ', '.join(cart)))
 	#sys.stdout.flush()
 
 class Gstate:
 
 	current_state = 0
 	cart = []
-	dict = {1: 'INIT', 2: 'ALCOHOL', 3: 'NON-ALCOHOL', 4:'FOOD', 5: 'UNDO', 6:'CHECKOUT', 7:'CASH', 8:'CREDIT'}
-
+	gestures = {1: 'INIT', 2: 'ALCOHOL', 3: 'NON-ALCOHOL', 4:'FOOD', 5: 'UNDO', 6:'CHECKOUT', 7:'CASH', 8:'CREDIT CARD'}
 
 	def GestureState(self, gesture):
-		if gesture == "init" and self.current_state == 0:
+		if gesture != 1 and self.current_state == 0:
+			printout(self,"Welcome!")
+
+		elif gesture == 1 and self.current_state == 0:
 			self.current_state = 1
 			printout(self, "What would you like to order?")
 
-		elif gesture == "alcohol" and self.current_state == 1:
-			self.cart.append("alcoholic-drink")
-			printout(self, "Alkohol drink added!")
+		elif gesture == 2 and self.current_state == 1:
+			self.cart.append(self.gestures[gesture])
+			printout(self, "Alcoholic drink added!")
 
-		elif gesture == "non-alcohol" and self.current_state == 1:
-			self.cart.append("non-alcoholic-drink")
-			printout(self, "Non-alkohol drink added!")
+		elif gesture == 3 and self.current_state == 1:
+			self.cart.append(self.gestures[gesture])
+			printout(self, "Non-alcohol drink added!")
 
-		elif gesture == "food" and self.current_state == 1:
-			self.cart.append("food")
-			printout(self, "food added!")
+		elif gesture == 4 and self.current_state == 1:
+			self.cart.append(self.gestures[gesture])
+			printout(self, "{} added.".format(self.gestures[gesture]))
 
-		elif gesture == "undo" and self.current_state == 1 and len(self.cart) > 0:
+		elif gesture == 5 and self.current_state == 1 and len(self.cart) > 0:
+			removedItem = self.cart[len(self.cart)-1]
 			self.cart.pop()
-			printout(self, "removed last item.")
+			printout(self, "Removed {} from cart".format(removedItem))
 
-		elif gesture == "undo" and self.current_state == 1 and len(self.cart) == 0:
+		elif gesture == 5 and self.current_state == 1 and len(self.cart) == 0:
 			self.current_state = 0
 			printout(self, "Nothing more to remove, going back....... Welcome back")
 
-		elif gesture == "checkout" and self.current_state == 1:
+		elif gesture == 6 and self.current_state == 1:
 			self.current_state = 2
 			printout(self, "How would you like to pay?")
 
-		elif gesture == "cash" and self.current_state == 2:
+		elif gesture == 7 and self.current_state == 2:
+			printout(self, "You paid for: " + str(self.cart)[1:-1] + "and payed with " + self.gestures[gesture])
 			self.cart = []
 			self.current_state = 0
-			printout(self, "You paid for: " + str(self.cart)[1:-1] + "and payed with cash!")
 
-		elif gesture == "credit" and self.current_state == 2:
+		elif gesture == 8 and self.current_state == 2:
+			printout(self, "You paid for: " + str(self.cart)[1:-1] + "and payed with " + self.gestures[gesture])
 			self.cart = []
 			self.current_state = 0
-			printout(self, "You paid for: " + str(self.cart) + "and payed with card!")
 
-		elif gesture == "undo" and self.current_state == 2:
+		elif gesture == 5 and self.current_state == 2:
 			self.current_state = 1
 			printout(self, "Back to order!")
 
+		else:
+			printout(self, "Gesture {} not legit!".format(self.gestures[gesture]))
+			pass
+
 ############################################### TEST ##################################################
 
-gs = Gstate()
-gs.GestureState("init")
-time.sleep(1)
-gs.GestureState("alcohol")
-time.sleep(1)
-gs.GestureState("non-alcohol")
-time.sleep(1)
-gs.GestureState("checkout")
-time.sleep(1)
-gs.GestureState("cash")
+def timeToSleep():
+	time.sleep(0)
 
-gs.GestureState("init")
-time.sleep(1)
-gs.GestureState("alcohol")
-time.sleep(1)
-gs.GestureState("non-alcohol")
-time.sleep(1)
-gs.GestureState("checkout")
-time.sleep(1)
-gs.GestureState("cash")
+gs = Gstate()
+
+gs.GestureState(0)
+timeToSleep()
+gs.GestureState(1)
+timeToSleep()
+gs.GestureState(4)
+timeToSleep()
+gs.GestureState(1)
+timeToSleep()
+gs.GestureState(5)
+timeToSleep()
+gs.GestureState(6)
+timeToSleep()
+gs.GestureState(7)
 print "\n"
