71b5f4de257f18f22d03f4de066777011b45c289
Marcus Windmark <marcus.windmark@gmail.com>
2016-05-18 14:54:18 +0200

    Added rest of draft nn

--- training/training.py
+++ training/training.py
@@ -92,3 +92,87 @@ class NeuralNetwork:
 
     #Train using gradient descent
     train_step = tf.train.GradientDescentOptimizer(learnRate).minimize(cross_entropy)
+
+
+'''
+def train_nn(inputData, outputData):
+  # Build computation graph by creating nodes for input images and target output classes
+  # 10 elements input
+  # 8 output classes
+
+  # Network input 
+  x = tf.placeholder(tf.float32, [None, 10])
+
+  # Network weights
+  W = tf.Variable(tf.zeros([10, 8]))
+
+  # Network bias
+  b = tf.Variable(tf.zeros([8]))
+
+  # Regression model implementation
+  y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+  # To implement cross-entropy, a new placeholder is needed to input the correct answers
+  y_ = tf.placeholder(tf.float32, [None, 8])
+
+  # Cost function
+  cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+  # Train the model
+  learnRate = 0.9
+
+  #Train using gradient descent
+  train_step = tf.train.GradientDescentOptimizer(learnRate).minimize(cross_entropy)
+
+  #Train using RMSProp#
+  #train_step = tf.train.RMSPropOptimizer(learnRate, decay=0.9, momentum=0.0, epsilon=1e-10, use_locking=False, name='RMSProp')
+
+  #Train using AdamOptimizer#
+  #train_step = tf.train.AdamOptimizer(learnRate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-08, use_locking=False, name='Adam')
+
+  # Add accuracy checking nodes
+  tf_correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+  tf_accuracy = tf.reduce_mean(tf.cast(tf_correct_prediction, "float"))
+
+  # Init variables
+  init = tf.initialize_all_variables()
+
+  sess = tf.Session()
+  sess.run(init)
+
+  # Run each training operation with 1000 training examples
+  k=[]
+  saved=0
+  for i in range(350):
+    #sess.run(train_step, feed_dict={x: x_train, y_: y_train})
+    #result = sess.run(tf_accuracy, feed_dict={x: x_test, y_: y_test})
+    sess.run(train_step, feed_dict={x: inputData, y_: outputData})
+    result = sess.run(tf_accuracy, feed_dict={x: inputData, y_: outputData})
+    if (i % 25 == 0):
+      print("Run {},{}".format(i,result))
+    k.append(result)
+
+  # Evaluate model
+  correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+  k=np.array(k)
+  print(np.where(k==k.max()))
+  print("Max accuracy: {}".format(k.max()))
+  print(' ')
+  print('NN training Validation :: Done.\n')
+
+
+
+
+label_vector = []
+for n in range(0,len(label_vector_strings)):
+  index = dict[label_vector_strings[n]]
+  label_array_representation = [0, 0, 0, 0, 0, 0, 0, 0]
+  label_array_representation[index] = 1
+  label_vector.append(label_array_representation)
+
+train_nn(input_vector, label_vector)
+
+#train_splitval(input_vector, label_vector_strings)
+'''
