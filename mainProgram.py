f2ff954e4a564c0fb1b2bff89585b98d8ee0f2a4
Christofer Lind <lind.christofer@gmail.com>
2016-05-18 13:33:42 +0200

    draft of the main program

---
+++ mainProgram.py
@@ -0,0 +1,24 @@
+import time, sys
+import states
+
+
+
+# Get input from Leap Motion
+def getDataLeapMotion():
+    pass
+
+# Sample input every ½ seconds
+def sampleData():
+    pass
+
+# Import classifier
+def importClassifer():
+    pass
+
+# Throw samples at classifier
+def useClassifier():
+    pass
+
+# Run states.py with gesture
+def runUI():
+    pass
