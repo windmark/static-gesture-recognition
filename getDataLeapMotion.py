9eb43048320f23e37e47cae332e51bef1fa8fb69
Christofer Lind <lind.christofer@gmail.com>
2016-05-24 12:23:24 +0200

    added comments

--- getDataLeapMotion.py
+++ getDataLeapMotion.py
@@ -3,7 +3,10 @@ sys.path.append("/Users/admin/Desktop/LeapSDK/lib/")
 import Leap
 from Leap import Finger
 
-
+'''
+Listener
+Connects to the Leap Motion and extracts the data.
+'''
 class Listener(Leap.Listener):
     left_hand_position = [-999, -999, -999]
     right_hand_position = [-999, -999, -999]
@@ -29,9 +32,14 @@ class Listener(Leap.Listener):
                 finger = Finger(pointable)
                 self.finger_positions[5*hand+finger.type] = [pointable.tip_position[0], pointable.tip_position[1], pointable.tip_position[2]]
 
-                #print("%s %f, %f, %f" % (hand, pointable.tip_position[0], pointable.tip_position[1], pointable.tip_position[2]))
 
-
+'''
+start()
+Returns in format: [x1,d2,y3],[x4,d5,y6],[[x7,d8,9y],[x1,d2,y3],...[xn,dn,yn]]
+Where the first list is the position of the left palm.
+The second list is the position of the right palm.
+The nested list contains the position of each finger.
+'''
 def start():
     # Create a sample listener and controller
     listener = Listener()
@@ -46,28 +54,31 @@ def start():
     fingers = []
 
     # Keep this process running until Enter is pressed
-    
     print "Press Enter to save"
     try:
         sys.stdin.readline()
     except KeyboardInterrupt:
         pass
     finally:
-        # Remove the sample listener when done
-        #print listener.left_hand_position
+        #Save the data into lists, from numpy.arrays
         for x in range(3):
             leftPalm.append(listener.left_hand_position[x])
         for x in range(3):
             rightPalm.append(listener.right_hand_position[x])
         for x in range(10):
             fingers.append(listener.finger_positions)
+
+        # Remove the sample listener when done
         controller.remove_listener(listener)
-        #print returnValues
-    #Return in format: [1,2,3],[4,5,6],[[7,8,9],[1,2,3],...[x,d,y]]
+
     returnValues.append(leftPalm)
     returnValues.append(rightPalm)
     returnValues.append(fingers)
     return returnValues
 
+'''
+getDataLeapMotion connects to the Leap Motion and extracts the data:
+position of palms and fingers
+'''
 if __name__ == "__main__":
     start()
